8d0605748a5edc2f1cd7dbd5910087393ba570ed
Your Namenayandeep-09 <nayandeepchouhan864@gmail.com>
2026-08-20 23:24:15 +0530

    new java repo created

---
+++ Statements.java
@@ -0,0 +1,10 @@
+public class Statements {
+    public static void main(String args[]) {
+        int num = 10;
+
+        if (num % 2 == 0)
+            System.out.println("It is even number");
+        else
+            System.out.println("It is odd number");
+}
+}
