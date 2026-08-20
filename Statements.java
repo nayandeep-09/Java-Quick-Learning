645147998ba61a0b1f1d5cc04665644864a8b249
Your Namenayandeep-09 <nayandeepchouhan864@gmail.com>
2026-08-21 02:32:51 +0530

    statements

--- Statements.java
+++ Statements.java
@@ -1,10 +1,22 @@
 public class Statements {
     public static void main(String args[]) {
-        int num = 10;
 
+        //If else statement
+        int num = 10;
         if (num % 2 == 0)
             System.out.println("It is even number");
         else
             System.out.println("It is odd number");
-}
+
+        // ternary operator
+        int num1 = 10;
+        int num2 = 20;
+        int j;
+        if (num1 > num2)
+            j=100;
+        else
+            j=0;
+        //  as the 10 was not greater than 20 so else part will be executed and the value of j will be 0.
+        System.out.println(j);
+    }
 }
