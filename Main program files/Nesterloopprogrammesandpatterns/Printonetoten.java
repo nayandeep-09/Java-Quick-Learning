4b9c147b502743a5a63b19e07fb8e7efaaca6a13
Your Namenayandeep-09 <nayandeepchouhan864@gmail.com>
2026-08-28 16:57:55 +0530

    githubdirectoryedited and folders created nested loop

--- Main program files/Nesterloopprogrammesandpatterns/Printonetoten.java
+++ Main program files/Nesterloopprogrammesandpatterns/Printonetoten.java
@@ -1,13 +1,16 @@
 public class Printonetoten {
+
     public static void main(String args[]) {
+
         // Print numbers 1 to 10 using a while loop
         int a = 1;
+
         while (a <= 10) {
             System.out.print(a + " ");
             a++;
         }
 
-        System.out.println("");
+        System.out.println();
 
         // Print numbers 10 to 1 using a for loop
         for (int b = 10; b >= 1; b--) {
